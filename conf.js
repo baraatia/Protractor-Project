7c4d18233ffa785ed1ece81000be448f14927828
baraaalsafady <baraaalsafady@baraas-macbook-air.home>
2019-03-10 15:48:26 +0200

    First commit

---
+++ conf.js
@@ -0,0 +1,18 @@
+'use strict';
+exports.config = {
+    baseUrl: 'http://www.way2automation.com/angularjs-protractor/banking/',
+    directConnect: true,
+    capabilities: {
+        browserName: 'chrome',
+        specs: 'specs/bankManagerSpec.js',
+    },
+    framework: 'jasmine',
+    useAllAngular2AppRoots: true,
+    allScriptsTimeout: 20000,
+    getPageTimeout: 20000,
+    restartBrowserBetweenTests: true,
+    onPrepare: function () {
+        browser.driver.manage().timeouts().implicitlyWait(10000);
+        browser.driver.manage().window().maximize();
+    }
+};
